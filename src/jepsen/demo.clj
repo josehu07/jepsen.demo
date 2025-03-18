50db334ac57f7112acee86ea0ac4e18198a0bddf
Guanzhou Hu <me@josehu.com>
2025-03-17 21:22:34 -0500

    add client and checker demo

--- src/jepsen/demo.clj
+++ src/jepsen/demo.clj
@@ -1,18 +1,32 @@
 (ns jepsen.demo
   (:require [clojure.tools.logging :refer :all]
             [clojure.string :as str]
-            [jepsen [cli :as cli]
+            [verschlimmbesserung.core :as vsrg]
+            [jepsen
+             [cli :as cli]
+             [client :as client]
              [control :as ctrl]
              [db :as db]
-             [tests :as tests]]
+             [generator :as gen]
+             [tests :as tests]
+             [checker :as checker]]
             [jepsen.control.util :as util]
-            [jepsen.os.ubuntu :as ubuntu]))
+            [jepsen.os.ubuntu :as ubuntu]
+            [jepsen.checker.timeline :as timeline]
+            [slingshot.slingshot :refer [try+]]
+            [knossos.model :as model])
+  (:import [knossos.model Model]))
 
 (defn spy> [val msg] (prn msg val) val)
 (defn spy>> [msg val] (spy> val msg))
 
-(def etcddir "/home/jepsen/etcd")
-(def etcdbin (str etcddir "/bin"))
+(defn str2num
+  "Parses a string to a Long. Passes through `nil`."
+  [s]
+  (when s (parse-long s)))
+
+(def etcddir "/home/jepsen/etcd-v3.1")
+(def etcdbin etcddir)
 (def etcddata (str etcddir "/data"))
 (def etcdwal (str etcddir "/wal"))
 (def etcdexec "etcd")
@@ -20,20 +34,30 @@
 (def logfile (str etcddir "/jepsen-etcd.log"))
 (def pidfile (str etcddir "/jepsen-etcd.pid"))
 
-(defn node-url
-  "An HTTP url for connecting to a node on a particular port."
-  [node port]
-  (str "http://" node ":" port))
+(defn compose-url
+  "An HTTP url composed of an address and a port."
+  [addr port]
+  (str "http://" addr ":" port))
 
 (defn peer-url
   "The HTTP url for other peers to talk to a node."
   [node]
-  (node-url node 2380))
+  (compose-url node 2380))
+
+(defn listen-peer-url
+  "The HTTP url to listen on for peers."
+  []
+  (compose-url "0.0.0.0" 2380))
 
 (defn client-url
   "The HTTP url clients use to talk to a node."
   [node]
-  (node-url node 2379))
+  (compose-url node 2379))
+
+(defn listen-client-url
+  "The HTTP url to listen on for clients."
+  []
+  (compose-url "0.0.0.0" 2379))
 
 (defn initial-cluster
   "Constructs an initial cluster string for a test, like
@@ -50,7 +74,7 @@
   (reify db/DB
     (setup! [_ test node]
       ;; (ctrl/su (let [url (format "https://github.com/etcd-io/etcd/releases/download/%s/etcd-%s-linux-amd64.tar.gz" version version)]
-      ;;            (util/install-archive! url "/opt/etcd"))))
+      ;;            (util/install-archive! url "/opt/etcd")))
       (info node "using etcd at" etcddir)
 
       (util/start-daemon!
@@ -62,11 +86,11 @@
        :--name                         (name node)
        :--data-dir                     etcddata
        :--wal-dir                      etcdwal
-       :--listen-peer-urls             (peer-url   node)
-       :--listen-client-urls           (client-url node)
+       :--listen-peer-urls             (listen-peer-url)
+       :--initial-advertise-peer-urls  (peer-url node)
+       :--listen-client-urls           (listen-client-url)
        :--advertise-client-urls        (client-url node)
        :--initial-cluster-state        :new
-       :--initial-advertise-peer-urls  (peer-url node)
        :--initial-cluster              (initial-cluster test))
 
       (Thread/sleep 10000))
@@ -84,14 +108,61 @@
     db/LogFiles
     (log-files [_ test node] [logfile])))
 
+(defrecord Client [conn]
+  client/Client
+
+  (open! [this test node]
+    (assoc this :conn (vsrg/connect (client-url node) {:timeout 5000})))
+
+  (setup! [this test])
+
+  (invoke! [_ test op]
+    (case (:f op)
+      :read (assoc op
+                   :value (str2num (vsrg/get conn "foo"))
+                   :type :ok)
+      :write (do (vsrg/reset! conn "foo" (:value op))
+                 (assoc op
+                        :type :ok))
+      :cas (try+
+            (let [[old new] (:value op)]
+              (assoc op
+                     :type (if (vsrg/cas! conn "foo" old new) :ok :fail)))
+            (catch [:errorCode 100] _
+              (assoc op
+                     :type :fail
+                     :error :not-found)))))
+
+  (teardown! [this test])
+
+  (close! [_ test]))
+
+(defn r   [_ _] {:type :invoke, :f :read, :value nil})
+(defn w   [_ _] {:type :invoke, :f :write, :value (rand-int 8)})
+(defn cas [_ _] {:type :invoke, :f :cas, :value [(rand-int 8) (rand-int 8)]})
+
+(def generator-params (->> (gen/mix [r w cas])
+                           (gen/stagger 1)
+                           (gen/nemesis nil)
+                           (gen/time-limit 15)))
+
+(def checker-params (checker/compose
+                     {:linearizability (checker/linearizable
+                                        {:model     (model/cas-register)
+                                         :algorithm :linear})
+                      :timeline-render (timeline/html)}))
+
 (defn etcd-test
   "A simple test over an etcd database."
   [opts]
   (-> tests/noop-test
-      (merge opts {:name "etcd"
-                   :os   ubuntu/os
-                   :db   (etcd-db "version.not.used")
-                   :pure-generators true})))
+      (merge opts {:name      "etcd"
+                   :os        ubuntu/os
+                   :db        (etcd-db "version.not.used")
+                   :client    (Client. nil)
+                   :pure-generators true
+                   :generator generator-params
+                   :checker   checker-params})))
 
 (defn -main
   "Main entrance to the test."
